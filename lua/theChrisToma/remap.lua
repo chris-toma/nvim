eedf3b00b25442b9dc374891cde0a140920e45f1
TheChrisToma <cristian.scribul@gmail.com>
2024-02-01 10:09:30 +0200

    Add fugitive and undotree

--- lua/theChrisToma/remap.lua
+++ lua/theChrisToma/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('n', '<Leader>ps', [[:w<CR>:Ex<CR>]], { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
