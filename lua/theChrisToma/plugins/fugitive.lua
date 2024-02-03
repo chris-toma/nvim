7a35e8165f751f074180cfee0c4d6b39edb6c5c8
TheChrisToma <cristian.scribul@gmail.com>
2024-02-03 12:29:07 +0200

    Add more commands

--- lua/theChrisToma/plugins/fugitive.lua
+++ lua/theChrisToma/plugins/fugitive.lua
@@ -2,5 +2,9 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-	end	
+		vim.api.nvim_set_keymap('n', '<leader>gp', ':Git pull<CR>', { noremap = true, silent = true })
+		vim.api.nvim_set_keymap('n', '<leader>gP', ':Git push<CR>', { noremap = true, silent = true })
+		vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
+
+	end
 }
